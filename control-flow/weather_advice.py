c5bfff71b14074aee1c8ada44b9ae5a4c1ba443b
EslamMahmoud001 <Eslam.Mahmoud767@gmail.com>
2024-09-20 18:48:40 +0300

    Week 4 Scripts

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -3,12 +3,10 @@
 Weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
 if Weather == "sunny":
-    text = f'Wear a t-shirt and sunglasses.'
+    print('Wear a t-shirt and sunglasses.')
 elif Weather == "rainy":
-    text = f"Don't forget your umbrella and a raincoat."
+    print("Don't forget your umbrella and a raincoat.")
 elif Weather == "cold":
-    text = f'Make sure to wear a warm coat and a scarf.'
+    print('Make sure to wear a warm coat and a scarf.')
 else: 
-    text = "Sorry, I don't have recommendations for this weather."
-    
-print(text)
+    print("Sorry, I don't have recommendations for this weather.")
