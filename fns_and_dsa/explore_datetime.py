b758673312fa009db964114cdd41865f2fe35540
EslamMahmoud001 <Eslam.Mahmoud767@gmail.com>
2024-09-24 17:22:49 +0300

    Week 5 Scripts

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,7 +3,7 @@ from datetime import date, timedelta, datetime
 def display_current_datetime():
     """ A function to display current date and time. """
     current_date = datetime.now()
-    current_date_time = current_date.strftime("%Y-%m-%d\s, %H:%M:%S")
+    current_date_time = current_date.strftime("%Y-%m-%d\s*%H:%M:%S")
     return print(f"Current date and time: {current_date_time}")
 display_current_datetime()
 def calculate_future_date():
