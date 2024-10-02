5ab6c4be8935a50da80dba412a8dbccfb4dc8c4a
EslamMahmoud001 <Eslam.Mahmoud767@gmail.com>
2024-10-02 10:18:26 +0300

    Week 6 Scripts

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,11 +1,11 @@
-class Book():
+class Book:
     def __init__(self, title, author) -> None:
         self.title = title
         self.author = author
         self._is_checked_out = False
     
         
-class Library():
+class Library:
     
     def __init__(self):
         self.__books = []
